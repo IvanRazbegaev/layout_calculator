c7d2fedb2a0c8062e85cf2d02af1e0e85c8dbecf
Ivan Razbegaev <sinnless13@gmail.com>
2021-10-01 00:15:11 +0300

    lesson 01 done

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = "";
+let screens = 0;
+let screenPrice = 1;
+let rollback = "";
+let fullPrice = 10;
+let adaptive = true;
+
+alert("Welcome to Glo Academy JS course!");
+console.log("Have a nice day!")
